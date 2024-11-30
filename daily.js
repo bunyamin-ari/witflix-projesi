122f679249afc2f4ef4a030aa71dbe489d6005e3
bunyamin-ari <bunyamin4720@gmail.com>
2024-11-30 18:09:34 +0300

    header a witflix added

--- daily.js
+++ daily.js
@@ -1,9 +1,11 @@
 // String ifadeler içeren bir array
-const phrases = ["Merhaba!", "Nasılsınız?", "JavaScript öğreniyorum!", "Bugün harika bir gün!", "Kod yazmak çok eğlenceli!"];
+const phrases = ["go to theatre with yusuf","spend time with your parents","call Mert Ölmez","go to tunnel aquarium","call Emrah","Watch a movie with Yusuf","test your programming process","think positively for a while","fix the treadmill","buy clothes","call şeyma","call rümeysa","call hamis","call emine","call recai"];
 
 // Rastgele bir ifade seçmek için array'in uzunluğunu kullanarak bir index oluşturuyoruz
 const randomIndex = Math.floor(Math.random() * phrases.length);
 
 // Rastgele seçilen ifadeyi konsola yazdırıyoruz
+
+
 console.log(phrases[randomIndex]);
 
